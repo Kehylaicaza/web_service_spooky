f0d605431a6fd9b9438055a3f6cc6d7953a0be12
Kehylaicaza <keilacicaza@gmail.com>
2019-01-22 00:42:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-ar http = require('http'),
+var http = require('http'),
 express  = require('express'),
 bodyParser  = require('body-parser');
 
@@ -30,3 +30,102 @@ app.use(enterCD);
 
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/verUsers', (req, res, next) => {
+    const client = new pg.Client(conString);
+    client.connect(function (err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+            return res.status(500).json({
+                success: false,
+                data: err
+            });
+        }
+
+        client.query('SELECT * FROM usuario', function (err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            console.log(result);
+            client.end();
+
+            return res.json(result.rows);
+
+        });
+    });
+
+});
+app.post('/createUser', (req, res) => {
+    var client = new pg.Client(conString);
+    client.connect(function(err) {
+        if(err) {
+            return console.error('could not connect to postgres', err);
+            return res.status(500).json({success: false, data: err});
+        }
+        
+        console.log("prueba"+util.inspect(req,false,null));
+        
+        client.query("INSERT INTO  usuario  (name,usuario,pass,sexo,edad,contacto) VALUES ('"+req.body.name+"', '"+req.body.user+"', '"+req.body.pass+"', '"+req.body.sexo+"', '"+req.body.edad+"', '"+req.body.contacto"');", function(err, result) {
+            if(err) {
+                return console.error('error running query', err);
+            }
+        
+          
+            client.end();
+            return res.json(result.rows);
+            
+        });
+        
+    });
+});
+
+app.put('/actualizarInformacion',(req,res)=>{
+    var client = new pg.Client(conString);
+    var id=req.body.id;
+    
+    client.connect(function(err) {
+        if(err) {
+            return console.error('could not connect to postgres', err);
+            return res.status(500).json({success: false, data: err});
+        }
+  
+        client.query("UPDATE usuario SET edad='"+req.body.edad+"' WHERE id='" + id + "';", function(err, result) {
+            
+            if(err) {
+                  return console.error('error running query', err);
+            }
+            
+           
+            client.end();
+            return res.json(result);
+        });
+     });
+});
+
+
+
+app.delete('/deleteUser',(req,res)=>{
+    var client = new pg.Client(conString);
+    var id=req.body.id;
+  
+    client.connect(function(err) {
+       if(err) {
+           return console.error('could not connect to postgres', err);
+           return res.status(500).json({success: false, data: err});
+       }
+
+       client.query('DELETE FROM usuario WHERE id = $1', [1], function(err, result) {
+          
+           if(err) {
+               return console.error('error running query', err);
+           }
+          
+           //console.log(result);
+            client.end();
+           return res.json(result);
+       });
+   });
+  
+  
+});
+ 
