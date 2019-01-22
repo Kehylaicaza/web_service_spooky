fff6fadd70e114e35d7208cd2cb4e468638179ac
Kehylaicaza <keilacicaza@gmail.com>
2019-01-22 15:11:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,29 @@ var http     = require('http'),
 	express  = require('express'),
 	parser   = require('body-parser');
 
-
+var multer = require('multer'); 
 const pg = require('pg');
 pg.defaults.ssl=true;
 var conString = "postgres://uhylhutdyevcbs:e6283c49398c23069debbac422b5c6326f63821f71d0f59d5c93672bfa9b98cc@ec2-54-235-68-3.compute-1.amazonaws.com:5432/dfmd7unrd6bl2t";
 
+function permitirCrossDomain(req, res, next) {
+  //en vez de * se puede definir SÓLO los orígenes que permitimos
+  res.header('Access-Control-Allow-Origin', '*'); 
+  //metodos http permitidos para CORS
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); 
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+
 var app = express();
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-app.set('port', process.env.PORT || 8080);
+ app.use(express.static('public'));
+app.use(permitirCrossDomain);
+
+
+app.listen(process.env.PORT || 8080, function(){console.log("the server is running");});
 
 
 app.get('/', (req, res) => res.send('Hello World!'));
