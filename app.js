edcde748de932fe16890de52c48f7f3179c7b213
Kehylaicaza <keilacicaza@gmail.com>
2019-01-21 22:05:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,18 @@
-var http = require('http'),
-express  = require('express'),
-bodyParser   = require('body-parser');
+var http     = require('http'),
+	express  = require('express'),
+	parser   = require('body-parser');
 
-var multer = require('multer'); 
-const pg    = require('pg');
 
-pg.defaults.ssl = true;
+const pg = require('pg');
+pg.defaults.ssl=true;
 
 var conString = "postgres://uhylhutdyevcbs:e6283c49398c23069debbac422b5c6326f63821f71d0f59d5c93672bfa9b98cc@ec2-54-235-68-3.compute-1.amazonaws.com:5432/dfmd7unrd6bl2t";
-var express = require('express');
-var http = require('http'),
-    formidable = require('formidable'),
-    util = require('util'),
-    fs   = require('fs-extra');
-function permitirCrossDomain(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); 
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
 
 var app = express();
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 8080);
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-app.use(express.static('public'));
-app.use(permitirCrossDomain);
 
 
 app.get('/', function (req, res, next) {
