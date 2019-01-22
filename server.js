8cc45401827d662dbac49fa67b45d4492f70abbd
Kehylaicaza <keilacicaza@gmail.com>
2019-01-22 15:16:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-var http     = require('http'),
+
+var http = require('http'),
 	express  = require('express'),
-	parser   = require('body-parser');
+	bodyParser   = require('body-parser');
 
 var multer = require('multer'); 
-const pg = require('pg');
-pg.defaults.ssl=true;
+const pg    = require('pg');
+
+pg.defaults.ssl = true;
+
 var conString = "postgres://uhylhutdyevcbs:e6283c49398c23069debbac422b5c6326f63821f71d0f59d5c93672bfa9b98cc@ec2-54-235-68-3.compute-1.amazonaws.com:5432/dfmd7unrd6bl2t";
 
+var express = require('express');
+var http = require('http'),
+    formidable = require('formidable'),
+    util = require('util'),
+    fs   = require('fs-extra');
 function permitirCrossDomain(req, res, next) {
   //en vez de * se puede definir SÓLO los orígenes que permitimos
   res.header('Access-Control-Allow-Origin', '*'); 
@@ -16,11 +24,12 @@ function permitirCrossDomain(req, res, next) {
   next();
 }
 
-
 var app = express();
-app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
- app.use(express.static('public'));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
 app.use(permitirCrossDomain);
 
 
