7653dc55f1cea636b35ff08797857dad5eaeed43
Kehylaicaza <keilacicaza@gmail.com>
2019-01-22 16:14:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use(express.static('public'));
 app.use(permitirCrossDomain);
 
 
- app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.get('/', function(req, res){
+   res.redirect('/verUsers');
 });
 
 
