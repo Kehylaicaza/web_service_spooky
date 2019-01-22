d279b8548815974a8439be4399cdf45dba2487e5
Kehylaicaza <keilacicaza@gmail.com>
2019-01-21 20:39:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ app.use(parser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 8080);
 
 
+app.get('/', function (req, res, next) {
+    pg.connect(connectionString,function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+       client.query('SELECT * FROM usuario where id = $1', [1],function(err,result) {
+           done(); // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
+    });
+});
+
 app.get('/verUsers', (req, res, next) => {
     const client = new pg.Client(conString);
     client.connect(function (err) {
