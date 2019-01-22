c0edae6b31da49de1c7cb40fb0870cdd4ac85df5
Kehylaicaza <keilacicaza@gmail.com>
2019-01-22 00:41:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
 
+ar http = require('http'),
+express  = require('express'),
+bodyParser  = require('body-parser');
+
+var multer = require('multer'); 
+const pg    = require('pg');
+
+pg.defaults.ssl = true;
+var conString = "postgres://uhylhutdyevcbs:e6283c49398c23069debbac422b5c6326f63821f71d0f59d5c93672bfa9b98cc@ec2-54-235-68-3.compute-1.amazonaws.com:5432/dfmd7unrd6bl2t";
+var express = require('express');
+var http = require('http'),
+    formidable = require('formidable'),
+    util = require('util'),
+    fs   = require('fs-extra');
+
+function enterCD(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE'); 
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+var app = express();
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static('public'));
+app.use(enterCD);
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
